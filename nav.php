5678f5ded5501fd55edee845a75d43d80888ff0e
Sunjida Nourin Shathi <91051092+SunjidaShathi@users.noreply.github.com>
2023-04-28 11:51:54 +0600

    Delete nav.php

--- nav.php
+++
@@ -1,5 +0,0 @@
- <ul>
-        <li><a href="addProduct.php"> Add Money</a></li>
-        <li><a href="showAllProducts.php"> Show all Money</a></li>
-        <li><a href="searchProduct.php"> Search Money</a></li>
-</ul>
